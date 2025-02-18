a5a8d4e57e50279108421196d3a580531ebd80bb
Carlos Andres Molano Manquillo <cmolano@bancolombia.com.co>
2025-02-17 23:00:10 -0500

    se agregan validaciones DTO y consulta por id prestamo con ultimos tres prestamos

---
+++ src/main/java/com/bancolombia/repasoreto/config/AuditoriaInterceptor.java
@@ -0,0 +1,25 @@
+package com.bancolombia.aplicacioncuenta.config;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class AuditoriaInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+        System.out.println("Petición interceptada: " + request.getMethod());
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+            throws Exception {
+        System.out.println("Petición completada: " + request.getMethod() + " " + request.getRequestURI());
+    }
+
+}
---
+++ src/main/java/com/bancolombia/repasoreto/config/GlobalExceptionHandler.java
@@ -0,0 +1,49 @@
+package com.bancolombia.aplicacioncuenta.config;
+
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> manejarArgumentoInvalido(IllegalArgumentException exception) {
+        return new ResponseEntity<>("Argumento inválido: " + exception.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(IllegalStateException.class)
+    public ResponseEntity<String> manejarEstadoIlegal(IllegalStateException exception) {
+        return new ResponseEntity<>("Estado ilegal: " + exception.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(NullPointerException.class)
+    public ResponseEntity<String> manejarNullPointer(NullPointerException exception) {
+        return new ResponseEntity<>("Error inesperado: " + exception.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> manejarErroresInesperados(Exception exception) {
+        return new ResponseEntity<>("Error inesperado: " + exception.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> manejarValidaciones(MethodArgumentNotValidException exception) {
+        Map<String, String> errores = new HashMap<>();
+
+        exception.getBindingResult().getAllErrors().forEach((error) -> {
+            String parametro = ((FieldError) error).getField();
+            String mensaje = error.getDefaultMessage();
+            errores.put(parametro, mensaje);
+        });
+
+        return new ResponseEntity<>(errores, HttpStatus.BAD_REQUEST);
+    }
+}
